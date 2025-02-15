a0e204cb0a1b7242cd49c854b8278bcd1412ebaf
slxshx <leon.linss@outlook.de>
2025-02-15 15:55:17 +0100

    Works - Checks if a date is valid or not

--- DateCheckerApp/Program.cs
+++ DateCheckerApp/Program.cs
@@ -1,9 +1,70 @@
 ﻿using System;
-
-public class Program
+namespace DateCheckerApp
 {
-    public static void Main(string[] args)
+    public class Program
     {
-        Console.WriteLine("Hello, World!");
+        public static void Main(string[] args)
+        {
+            string again = "y";
+            string[] monthNames = new string[] { "Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember" };
+
+            while (again == "y")
+            {
+                Console.WriteLine("Bitte gebe 3 Zahlen für ein Datum ein.");
+                Console.Write("Tag: ");
+                int day = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Monat: ");
+                int month = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Jahr: ");
+                int year = Convert.ToInt32(Console.ReadLine());
+
+                ValidDateCheck vDC = new ValidDateCheck();
+                bool result = vDC.ValidDate(day, month, year);
+                try
+                {
+                    Console.WriteLine($"{day}. {monthNames[month - 1]} {year}.");
+                }
+                catch
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Es gibt nur 12 Monate..");
+                    Console.ReadKey();
+                    Console.ResetColor();
+                    Console.Clear();
+                    continue;
+                }
+                
+                Console.WriteLine("Datum wird geprüft..");
+                
+                Thread.Sleep(3000);
+
+                if (result == true)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Datum ist gültig.");
+                    Thread.Sleep(1000);
+                    Console.ResetColor();
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Datum ist ungültig.");
+                    Thread.Sleep(1000);
+                    Console.ResetColor();
+                }
+
+                Console.Write("Weiteres Datum prüfen y/n: ");
+                again = Console.ReadLine();
+
+                if (again == "y")
+                {
+                    Console.Clear();
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
     }
 }
---
+++ DateCheckerApp/ValidDateCheck.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace DateCheckerApp
+{
+	public class ValidDateCheck
+	{
+		public bool ValidDate(int day, int month, int year)
+		{
+			int[] monthDays = new int[] {31,28,31,30,31,30,31,31,30,31,30,31};
+
+			if (year < 0 || year > 10000)
+			{
+				return false;
+			}
+			if (month < 1 || month > 12)
+			{
+				return false;
+			}
+			if (month == 2 && year % 4 == 0 || year % 400 == 0 && year % 100 != 0)
+			{
+				monthDays[1] = 29;
+			}
+			if (monthDays[month] < day || day < 1)
+			{
+				return false;
+			}
+			return true;
+		}
+	}
+}
+
